90aeaa2b3a7c975779ef62181376b2928c2f9e5c
Anvar <anvar.ganiev99@gmail.com>
2023-10-22 15:13:03 -0400

    Fixes and formatting + added View Departments, roles, employees

--- index.js
+++ index.js
@@ -1,115 +1,134 @@
-const prompts = require('prompts');
-const cTable = require('console.table');
 const figlet = require('figlet');
-// importing the connection from the config folder
+// Prompt the user to select an option
+const enquirer = require('enquirer');
 const connection = require('./config/connection');
-// Connecting to database
+
 connection.connect(function (err) {
-    if (err) throw err;
-    console.log("connected as id " + connection.threadId);
-    // Create a figlet text to print on terminal screen "Employee Tracker"
-    figlet('Employee Tracker', function (err, data) {
-        if (err) {
-            console.log('Something went wrong...');
-            console.dir(err);
-            return;
-        }
-        console.log(data);
-    })
-    // Wait until employee tracker figlet is printed
-    setTimeout(() => {
-        promptUser();
-    });
+  if (err) throw err;
+  console.log("Connected as id " + connection.threadId);
+  promptUser();
 });
 
-async function promptUser() {
-	(async () => {
-	  const choices = [ // Choices for user to select from
-		{ title: 'View all departments', value: 'viewDepartments' },
-		{ title: 'View all roles', value: 'viewRoles' },
-		{ title: 'View all employees', value: 'viewEmployees' },
-		{ title: 'Add a department', value: 'addDepartment' },
-		{ title: 'Add a role', value: 'addRole' },
-		{ title: 'Add an employee', value: 'addEmployee' },
-		{ title: 'Update an employee role', value: 'updateEmployeeRole' },
-    { title: 'Update a role title', value: 'updateRoleTitle' },
-    { title: 'Delete a role', value: 'deleteRole' },
-    { title: 'Delete an employee', value: 'deleteEmployee' },
-    { title: 'Delete a department', value: 'deleteDepartment' },
-    { title: 'Update an employee manager', value: 'updateEmployeeManager' },
-    { title: 'View employees by manager', value: 'viewEmployeesByManager' },
-    { title: 'View employees by department', value: 'viewEmployeesByDepartment' },
-    { title: 'View the total utilized budget of a department', value: 'viewTotalUtilizedBudget' },
-		{ title: 'Exit', value: 'exit' },
-	  ];
-  
-    // Prompt user to select from choices
-	  const response = await prompts({
-		  type: 'select',
-		  name: 'action',
-		  message: 'What would you like to do?',
-		  choices: choices.map((choice, index) => ({
-		    title: `${index + 1}. ${choice.title}`,
-		    value: choice.value,
-		  })),
-	  });
+// List of choices for the user to select from
+const choices = [
+  { title: 'View all departments', value: 'viewDepartments' },
+  { title: 'View all roles', value: 'viewRoles' },
+  { title: 'View all employees', value: 'viewEmployees' },
+  { title: 'Add a department', value: 'addDepartment' },
+  { title: 'Add a role', value: 'addRole' },
+  { title: 'Add an employee', value: 'addEmployee' },
+  { title: 'Update an employee role', value: 'updateEmployeeRole' },
+  { title: 'Update a role title', value: 'updateRoleTitle' },
+  { title: 'Delete a role', value: 'deleteRole' },
+  { title: 'Delete an employee', value: 'deleteEmployee' },
+  { title: 'Delete a department', value: 'deleteDepartment' },
+  { title: 'Update an employee manager', value: 'updateEmployeeManager' },
+  { title: 'View employees by manager', value: 'viewEmployeesByManager' },
+  { title: 'View employees by department', value: 'viewEmployeesByDepartment' },
+  { title: 'View the total utilized budget of a department', value: 'viewTotalUtilizedBudget' },
+  { title: 'Exit', value: 'exit' },
+];
 
-	  console.log('response:', response);
-	  // Perform action based on user's choice
-    switch (response.action) {
-    case 'viewDepartments':
-      viewDepartments();
-      break;
-    case 'viewRoles':
-      viewRoles();
-      break;
-    case 'viewEmployees':
-      viewEmployees();
-      break;
-    case 'addDepartment':
-      addDepartment();
-      break;
-    case 'addRole':
-      addRole();
-      break;
-    case 'addEmployee':
-      addEmployee();
-      break;
-    case 'updateEmployeeRole':
-      updateEmployeeRole();
-      break;
-    case 'updateRoleTitle':
-      updateRoleTitle();
-      break;
-    case 'deleteRole':
-      deleteRole();
-      break;
-    case 'deleteEmployee':
-      deleteEmployee();
-      break;
-    case 'deleteDepartment':
-      deleteDepartment();
-      break;
-    case 'updateEmployeeManager':
-      updateEmployeeManager();
-      break;
-    case 'viewEmployeesByManager':
-      viewEmployeesByManager();
-      break;
-    case 'viewEmployeesByDepartment':
-      viewEmployeesByDepartment();
-    case 'exit':
-      figlet('Good bye!', function (err, data) {
-        if (err) {
+async function promptUser() {
+  const response = await enquirer.prompt({
+    type: 'select',
+    name: 'option',
+    message: 'Please select an option:',
+    choices: choices.map(choice => choice.title),
+  });
+
+  // Handle the selected option
+  const selectedOption = choices.find(choice => choice.title === response.option);
+  if (selectedOption) {
+    // Call the corresponding function or perform the desired action
+    switch (selectedOption.value) {
+      case 'viewDepartments':
+        viewDepartments();
+        break;
+      case 'viewRoles':
+        viewRoles();
+        break;
+      case 'viewEmployees':
+        viewEmployees();
+        break;
+      case 'addDepartment':
+        addDepartment();
+        break;
+      case 'addRole':
+        addRole();
+        break;
+      case 'addEmployee':
+        addEmployee();
+        break;
+      case 'updateEmployeeRole':
+        updateEmployeeRole();
+        break;
+      case 'updateRoleTitle':
+        updateRoleTitle();
+        break;
+      case 'deleteRole':
+        deleteRole();
+        break;
+      case 'deleteEmployee':
+        deleteEmployee();
+        break;
+      case 'deleteDepartment':
+        deleteDepartment();
+        break;
+      case 'updateEmployeeManager':
+        updateEmployeeManager();
+        break;
+      case 'viewEmployeesByManager':
+        viewEmployeesByManager();
+        break;
+      case 'viewEmployeesByDepartment':
+        viewEmployeesByDepartment();
+        break;
+      case 'viewTotalUtilizedBudget':
+        viewTotalUtilizedBudget();
+        break;
+      case 'exit':
+        figlet.text('Goodbye!', function (err, data) {
+          if (err) {
             console.log('Something went wrong...');
             console.dir(err);
             return;
-        }
-        console.log(data);
-      })
-      // Close the connection
-      connection.end();
-      break;
-	  }
-	})();
-}
+          }
+          console.log(data);
+        })
+        connection.end();
+        break;
+      default:
+        console.log('Invalid option');
+    }
+  } else {
+    console.log('Invalid option');
+  }
+};
+
+// View all departments
+function viewDepartments() {
+  connection.query('SELECT * FROM departments', function (err, res) {
+    if (err) throw err;
+    console.table(res);
+    promptUser();
+  });
+};
+
+// View all roles
+function viewRoles() {
+  connection.query('SELECT * FROM roles', function (err, res) {
+    if (err) throw err;
+    console.table(res);
+    promptUser();
+  });
+};
+
+// View all employees
+function viewEmployees() {
+  connection.query('SELECT * FROM employee', function (err, res) {
+    if (err) throw err;
+    console.table(res);
+    promptUser();
+  });
+};
